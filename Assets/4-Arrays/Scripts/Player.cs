8944b2e0237c9fc850aefef7c6bf69138236500a
ryanvaldes <ryanvaldes8@gmail.com>
2017-09-22 15:56:16 +1000

    Made Arrays game

---
+++ Assets/4-Arrays/Scripts/Player.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace Arrays
+{
+    public class Player : MonoBehaviour
+    {
+        public float movementSpeed = 20f;
+
+        private Rigidbody rigid;
+
+        // Use this for initialization
+        void Start()
+        {
+            rigid = GetComponent<Rigidbody>();
+        }
+       
+        // Update is called once per frame
+        void Update()
+        {
+        float inputH = Input.GetAxis("Horizontal");
+        float inputV = Input.GetAxis("Vertical");
+
+            float camY = Camera.main.transform.eulerAngles.y;
+            Quaternion rotation = Quaternion.Euler(0, camY, 0);
+
+        Vector3 inputDir = rotation * new Vector3(inputH, 0, inputV);
+        rigid.AddForce(inputDir * movementSpeed);
+        }
+        
+    }
+}
---
+++ Assets/4-Arrays/Scripts/Powerup.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace Arrays
+{
+    public class Powerup : MonoBehaviour
+    {
+        public float speedIncrease = 5f;
+        public float rotationSpeed = 30f;
+
+
+
+        void OnTriggerEnter(Collider other)
+        {
+            Player p = other.GetComponent<Player>();
+            if(p != null)
+            {
+                p.movementSpeed += speedIncrease;
+                Destroy(gameObject);
+            }
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            transform.Rotate(Vector3.up, rotationSpeed * Time.deltaTime, Space.World);
+        }
+    }
+}
