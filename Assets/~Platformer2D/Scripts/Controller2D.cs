8eb001583359877ddb70a6ae3f6acec1bfabb48c
ryanvaldes <ryanvaldes8@gmail.com>
2017-11-03 14:00:01 +1100

    added platformer 2d

---
+++ Assets/~Platformer2D/Scripts/Controller2D.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace Platformer2D
+{
+    [RequireComponent(typeof(Rigidbody))]
+    public class Controller2D : MonoBehaviour
+    {
+        public float acceleration = 20f;
+        public float jumpHeaight = 10f;
+
+        private Rigidbody rigid;
+
+        // Use this for initialization
+        void Start()
+        {
+            rigid = GetComponent<Rigidbody>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+
+}
